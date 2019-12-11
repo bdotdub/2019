163b5c95b44054f22eaadc8da5ec4905bd199907
Benny Wong <benny@bwong.net>
2019-12-10 23:52:22 -0500

    Start refactoring by moving to a main file and relative imports

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os, sys
+
+
+class Settings:
+    project_root: str
+
+    def __init__(self) -> None:
+        self.project_root = os.path.dirname(os.path.realpath(__file__))
+
+    def config_path_for(self, scope: str, filename: str) -> str:
+        return os.path.join(self.project_root, "config", scope, filename)
+
